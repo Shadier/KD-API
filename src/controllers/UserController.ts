abcdc94d21ea764b9871f1b618aca8015368a624
DanielCornejo <fdanielcm@gmail.com>
2019-09-24 00:42:24 -0500

    Make a POST to inserti USER

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,36 @@
 import { Router } from 'express'
-const User  = require('../models/user')
+const elUsuario  =    require('../models/user')
 
 export const userRouter = Router()
 userRouter.get('/', (req, res) => {
-	User.find((err, users) =>{
+	elUsuario.find((err, users) =>{
 		if(err) return res.status(500).send({message: 'Internal Server error'})
 		if(!users) return res.status(404).send({message: 'users not founded!'})
 		return res.status(200).send({users})
 	})
+})
+
+
+userRouter.post('/create',(req,res)=>{
+	const params= req.body;
+	console.log(params);
+	let usuario = new elUsuario();
+	usuario.name= params.name;
+	usuario.lastname=params.lastname;
+	usuario.email=params.email;
+	usuario.password= params.password;
+	usuario.role=params.role;
+	usuario.userIA= params.userIA;
+	usuario.remoteDays=params.remoteDays;
+	usuario.last_day=params.last_day;
+	usuario.report=params.report;
+	usuario.last_partner=params.last_partner;
+	usuario.save((err,usrsave)=>{
+		console.log({usrsave});
+		console.log(err);
+		if(err) return res.status(500).send({message: 'Internal Server error, User doesn´t saved'})
+		if(usrsave) res.status(200).send({client: usrsave})
+		else res.status(404).send({message: 'Actor not saved!'})
+	})
+
 })
