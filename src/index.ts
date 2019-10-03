6d325a760a203874bfab5df00acb8ae0d0e6e872
Gustavo Urbina <urbina_gustavo@hotmail.com>
2019-10-03 15:51:14 -0500

    backend finish

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import bodyParser from 'body-parser'
 import { userRouter } from './controllers/UserController'
 import { tagteamRouter } from './controllers/TagTeamController'
 import { codeRouter } from './controllers/CodeController'
+import {entrada} from './controllers/mailController'
 import mongoose from 'mongoose';
 
-
+const cors = require('cors');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/KitchenDuty', { useNewUrlParser: true })
 		.then(() => {
@@ -18,12 +19,18 @@ mongoose.connect('mongodb://localhost:27017/KitchenDuty', { useNewUrlParser: tru
 const port = process.env.port || 1337
 
 const app = express()
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use('/users', userRouter)
 app.use('/tagteams', tagteamRouter)
 app.use('/code', codeRouter)
+
+
+app.use('/mail', entrada)
+
+
 
 
 
